648f6eac2af896a01251e74633a661ec9ffcda96
4lberth <alberth.mayta@tecsup.edu.pe>
2025-04-16 08:49:57 -0500

    modificaciones en app.js

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const express = require('express');
+import express from 'express';
+import clientesRouter from './routes/clientes.js';
+import productosRouter from './routes/productos.js';
+
 const app = express();
 const port = 9000;
-
-import clientesRouter = require('./routes/clientes');
-import productosRouter = require('./routes/productos');
 
 app.use('/clientes', clientesRouter);
 app.use('/productos', productosRouter);
 
 app.get('/', (req, res) => {
-res.send('HOla desde la ruta raiz');
+  res.send('Hola desde la ruta raiz');
 });
 
 app.use(express.static('public'));
 
-app.listen(port, ()) => {
-console.log('Servidor corriendo en http://localhost:${port}');
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
+
